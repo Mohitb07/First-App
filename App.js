bda5d20687f6d3d9719fbcafa6e6064c8cd82970
mohitb07 <bmohit980@gmail.com>
2021-08-18 20:49:22 +0530

    modal state

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { Button, FlatList, StyleSheet, View } from 'react-native';
 
 import Item from './components/Item';
 import Input from './components/Input';
@@ -7,20 +7,26 @@ import Input from './components/Input';
 export default function App() {
 
   const [todoList, setTodoList] = useState([]);
-  
+  const [isAddMode, setIsAddMode] = useState(false)
 
-  function submitHandler(inputText,setInputText){
+  function submitHandler(inputText,setInputText){ 
       setTodoList((currentTodoList) => [...currentTodoList, {key: Math.random().toString(), value: inputText}]);    
       setInputText('');
+      setIsAddMode(false)
   }
 
   function removeItemHandler(itemId){
     setTodoList(currentTodoList => currentTodoList.filter(item => item.key !== itemId));
   }
 
+  function onCancelModal(){
+    setIsAddMode(false);
+  }
+
   return (
       <View style={styles.container}>       
-        <Input onSubmit={submitHandler}/>
+        <Button title="Add new item" onPress={() => setIsAddMode(true)}/>
+        <Input isVisible={isAddMode} onSubmit={submitHandler} onCancel={onCancelModal}/>
         <FlatList data={todoList}
           renderItem={itemData => <Item id={itemData.item.key} onDelete={removeItemHandler} title={itemData.item.value}/> }
         />
